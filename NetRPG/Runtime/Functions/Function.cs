e0bedf2e01af66afc8c578dd5ec061ce952d1b4b
SaschaS93 <43013380+SaschaS93@users.noreply.github.com>
2019-08-30 21:40:17 +0200

    Function.cs refactored

--- NetRPG/Runtime/Functions/Function.cs
+++ NetRPG/Runtime/Functions/Function.cs
@@ -1,42 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Reflection;
 
 namespace NetRPG.Runtime.Functions
 {
     class Function
     {
-        private String[] FunctionAliases = {};
+        private String[] FunctionAliases = { };
 
         //TODO: only add items to this list if it's used
         private static Dictionary<string, Function> List = new Dictionary<string, Function>
         {
         };
 
-        public static void AddFunctionReference(string Name, string Func) {
+        public static void AddFunctionReference(string functionIdentifier, string rpgFunctionName)
+        {
             Function result = null;
 
-            Name = Name.ToUpper();
+            functionIdentifier = functionIdentifier.ToUpper();
 
-            result = CreateFunction(Func); 
-       
-            if (result == null) {
-                Error.ThrowCompileError("Function '" + Func + "' does not exist in system.");
-            } else {
-                if (!List.ContainsKey(Name))
-                    List.Add(Name, result);
+            result = CreateFunction(rpgFunctionName);
+
+            if (result == null)
+            {
+                Error.ThrowCompileError("Function '" + rpgFunctionName + "' does not exist in system.");
+            }
+            else
+            {
+                if (!List.ContainsKey(functionIdentifier))
+                    List.Add(functionIdentifier, result);
             }
         }
 
-        private static Function CreateFunction(string Func)
+        private static Function CreateFunction(string rpgFunctionName)
         {
             List<Type> types = new List<Type>();
             Assembly assembly = Assembly.GetExecutingAssembly();
             foreach (Type type in assembly.GetTypes())
             {
                 // Check if it's a class from the function namespace
-                if(type == null || type.Namespace == null || !type.Namespace.StartsWith("NetRPG.Runtime.Functions") || type.BaseType != typeof(Function))
+                if (type == null || type.Namespace == null || !type.Namespace.StartsWith("NetRPG.Runtime.Functions") || type.BaseType != typeof(Function))
                 {
                     continue;
                 }
@@ -45,53 +48,54 @@ namespace NetRPG.Runtime.Functions
                 string prefix = type.Namespace.StartsWith("NetRPG.Runtime.Functions.BIF") ? "%" : "";
 
                 // Normalize RPG function and typename to find the correct function type
-                string normalizedFuncName = Func.ToUpper();
+                string normalizedFuncName = rpgFunctionName.ToUpper();
                 string normalizedTypeName = prefix + type.Name.ToUpper();
 
                 // Compare RPG function name with name of type and return instance of rpg function implementation if found
-                if(normalizedFuncName == normalizedTypeName) {
-                    Function function = (Function) Activator.CreateInstance(type);
+                if (normalizedFuncName == normalizedTypeName)
+                {
+                    Function function = (Function)Activator.CreateInstance(type);
                     return function;
                 }
 
                 // Check for alias
-                foreach(Attribute attribute in type.GetCustomAttributes())
+                foreach (Attribute attribute in type.GetCustomAttributes())
                 {
                     // If the current attribute is not a RPG function alias, then give next attribute
-                    if(attribute.GetType() != typeof(RPGFunctionAlias)) continue;
+                    if (attribute.GetType() != typeof(RPGFunctionAlias)) continue;
 
-                    RPGFunctionAlias rpgFunctionAlias = (RPGFunctionAlias) attribute;
+                    RPGFunctionAlias rpgFunctionAlias = (RPGFunctionAlias)attribute;
 
                     // Normalize RPG function alias name for comparisation
                     string normalizedRPGFunctionAlias = rpgFunctionAlias.Alias.ToUpper();
 
                     // Compare RPG function name with name of alias and return instance of rpg function implementation if found
-                    if(normalizedRPGFunctionAlias == normalizedFuncName)
+                    if (normalizedRPGFunctionAlias == normalizedFuncName)
                     {
-                        Function function = (Function) Activator.CreateInstance(type);
+                        Function function = (Function)Activator.CreateInstance(type);
                         return function;
                     }
                 }
             }
-        
+
             return null;
         }
 
-        public static bool IsFunction(string Name)
+        public static bool IsFunction(string name)
         {
-            return List.ContainsKey(Name.ToUpper());
+            return List.ContainsKey(name.ToUpper());
         }
 
-        public static Function GetFunction(string Name)
+        public static Function GetFunction(string name)
         {
-            Name = Name.ToUpper();
+            name = name.ToUpper();
 
-            if (List.ContainsKey(Name))
-                return List[Name];
+            if (List.ContainsKey(name))
+                return List[name];
             else
                 return null; //TODO: throw error
         }
-        
+
         public virtual object Execute(object[] Parameters)
         {
             return null;
